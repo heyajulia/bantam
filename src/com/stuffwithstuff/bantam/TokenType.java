e548694f54d8aca680783d11477e53ba20b72013
Julia <juliariety@gmail.com>
2021-02-16 16:12:55 +0100

    Replace with switch expression in punctuator()

--- src/com/stuffwithstuff/bantam/TokenType.java
+++ src/com/stuffwithstuff/bantam/TokenType.java
@@ -22,35 +22,21 @@ public enum TokenType {
      * identifier like '+', this will get its text.
      */
     public Character punctuator() {
-        switch (this) {
-            case LEFT_PAREN:
-                return '(';
-            case RIGHT_PAREN:
-                return ')';
-            case COMMA:
-                return ',';
-            case ASSIGN:
-                return '=';
-            case PLUS:
-                return '+';
-            case MINUS:
-                return '-';
-            case ASTERISK:
-                return '*';
-            case SLASH:
-                return '/';
-            case CARET:
-                return '^';
-            case TILDE:
-                return '~';
-            case BANG:
-                return '!';
-            case QUESTION:
-                return '?';
-            case COLON:
-                return ':';
-            default:
-                return null;
-        }
+        return switch (this) {
+            case LEFT_PAREN -> '(';
+            case RIGHT_PAREN -> ')';
+            case COMMA -> ',';
+            case ASSIGN -> '=';
+            case PLUS -> '+';
+            case MINUS -> '-';
+            case ASTERISK -> '*';
+            case SLASH -> '/';
+            case CARET -> '^';
+            case TILDE -> '~';
+            case BANG -> '!';
+            case QUESTION -> '?';
+            case COLON -> ':';
+            default -> null;
+        };
     }
 }
